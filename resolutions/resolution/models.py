4d380f4c695c14a300d2cf7967e5dc295d9500c1
Shannon Turner <shannon.v.turner@gmail.com>
2019-10-12 17:39:44 -0400

    Updated resolution to use str instead of unicode for py3

--- resolutions/resolution/models.py
+++ resolutions/resolution/models.py
@@ -7,5 +7,5 @@ class Resolution(models.Model):
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.text
